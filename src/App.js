7cea4cfa7c3c32249cbf090fcba16dd20a0e5f0c
RahmatSatyawan <rahmat.stywan@gmail.com>
2022-05-29 04:12:05 +0700

    try login/register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-        <Button variant="primary">Primary</Button>{' '}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Nav } from "./component/Nav/Nav";
+
+const App = () => {
+  return <Nav />;
+};
 
 export default App;
